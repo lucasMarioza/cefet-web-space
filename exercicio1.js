dfc09021ee83b4c7a4827f92315294276fb07530
LucasMarioza <lucasmarioza@gmail.com>
2021-06-22 19:17:42 -0300

    calculo

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,13 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+function calcular() {
+  const g = document.getElementById('constante').value;
+  const m1 = document.getElementById('massa1').value;
+  const m2 = document.getElementById('massa2').value;
+  const d = document.getElementById('distancia').value;
+
+  const resultado = (g * m1 * m2)/d;
+
+  document.getElementById('resultado').value = resultado
+}
