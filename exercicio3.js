b693b461be855525a6f0091197e5e03b0b6e78ae
LucasMarioza <lucasmarioza@gmail.com>
2021-06-22 23:12:31 -0300

    botao expandir

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,14 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+function alternarExpandido(evt) {
+    const botao = evt.currentTarget;
+    const paragrafo = botao.parentElement;
+    paragrafo.classList.toggle('expandido')
+    botao.textContent = botao.textContent === '+' ? '-' : '+';
+}
+
+const botoes = document.querySelectorAll('#aliens p button');
+botoes.forEach(botao => (
+    botao.addEventListener('click', alternarExpandido)
+));
